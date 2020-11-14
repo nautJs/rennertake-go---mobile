2f6b7829fc0fd380d06de15ae1398709cf831bee
Jeffesson Gomes <jeffessong4@gmail.com>
2020-11-14 02:08:13 -0300

    merge

--- App.js
+++ App.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Dimensions, SafeAreaView} from 'react-native';
+import Routes from './src/routes/routes';
+import { useFonts } from 'expo-font';
+import { Lato_100Thin,
+  Lato_100Thin_Italic,
+  Lato_300Light,
+  Lato_300Light_Italic,
+  Lato_400Regular,
+  Lato_400Regular_Italic,
+  Lato_700Bold,
+  Lato_700Bold_Italic,
+  Lato_900Black,
+  Lato_900Black_Italic } from '@expo-google-fonts/lato'
 
-import QRcode from './src/pages/empty_cart';
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_100Thin,
+    Lato_100Thin_Italic,
+    Lato_300Light,
+    Lato_300Light_Italic,
+    Lato_400Regular,
+    Lato_400Regular_Italic,
+    Lato_700Bold,
+    Lato_700Bold_Italic,
+    Lato_900Black,
+    Lato_900Black_Italic
+  })
 
-const App = () => <empty_cart />;
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>
+  }
 
-export default App;
+  return (
+    <Routes/>
+  );
+}
+
+
