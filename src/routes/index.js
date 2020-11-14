77a2b321ba675d4885018977d3634c320485ea7a
Jeffesson Gomes <jeffessong4@gmail.com>
2020-11-14 01:19:19 -0300

    version 1 (#14)
    Co-authored-by: mattwnne <mateusmateus85@gmail.com>

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'React';
+
+import { SafeAreaView, View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+const Cart = () => {
+  return(
+    <SafeAreaView >
+      <Text>
+          NENHUM PRODUTO AINDA ;-(
+        </Text>
+
+    </SafeAreaView>
+  )
+}
+
+
---
+++ src/routes/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+  },
+
+  ContainerTitle: {
+    height: 300,
+  },
+
+  Title: {
+    fontSize: 50,
+    fontWeight: '330',
+    }
+});
+
+
+
