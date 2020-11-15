7960bf47290ba3a81428e249bfbb9fc12bd2563d
Jeffesson Gomes <jeffessong4@gmail.com>
2020-11-14 22:20:03 -0300

    Changing the structure of the routes (#19)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,22 +7,34 @@ import Header from '../components/Header/';
 
 
 import TopBar from '../components/TopBar/';
+import ScannerCode from '../pages/ScannerCode';
 
 export default function Routes() {
     return (
-        <NavigationContainer>
-        <Navigator>
-            {/* <Screen name="Menu" component={Menu}/> */}
-            <Screen
-                name="Menu"
-                component={TopBar}
-                options={{
-                  headerShown: true,
-                  header: () => <Header showCancel={false} title="Qr Code"/>
-                }}
-            />
+    <NavigationContainer>
+      <Navigator>
+          {/* <Screen name="Menu" component={Menu}/> */}
+          {/* <Screen
+              name="Menu"
+              component={TopBar}
+              options={{
+                headerShown: true,
+                header: () => <Header showCancel={false} title="Qr Code"/>
+              }}
+          /> */}
+          <Screen
+              name="ScannerCode"
+              component={ScannerCode}
+              options={{
+                headerShown: true,
+                headerTransparent: true,
 
-        </Navigator>
+                header: (e) => <Header headerTransparent={true} showCancel={false} title="Escanear"/>
+              }}
+          />
+
+      </Navigator>
+
     </NavigationContainer>
     )
 }
